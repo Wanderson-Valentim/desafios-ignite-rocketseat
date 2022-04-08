a8fb7fc159e481d4c00749c99d8809fb0e568c14
Wanderson Valentim <wanderson200108@gmail.com>
2022-04-08 17:34:00 -0300

    relocating ignite challenges

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+import { api } from './services/api';
+import { GenreResponseProps, MovieProps } from './assets/Interfaces'
+import './styles/global.scss';
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar setSelectedGenreId={setSelectedGenreId} selectedGenreId={selectedGenreId}/>
+      <Content selectedGenre={selectedGenre} movies={movies}/>
+    </div>
+  )
+}
